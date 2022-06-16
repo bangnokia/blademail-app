951b17699776275b8f8af59da15b6c4b95305220
bangnokia <bangnokia@gmail.com>
2022-06-17 00:03:21 +0700

    work in progress

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,14 +4,7 @@ import Pricing from "~/components/pricing";
 import { useLoaderData } from '@remix-run/react';
 import { LoaderFunction } from "remix";
 
-export let loader: LoaderFunction = async () => {
-    let response = await fetch("https://lab.daudau.cc/api/apps/blade-mail/latest-release");
-
-    return response.json();
-}
-
 export default function Index() {
-    const release = useLoaderData();
     return (
         <>
             <div className="bg-white">
