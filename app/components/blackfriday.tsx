3e73cf591b45259c19f6d2e6b920d0c57abbbc33
bangnokia <bangnokia@gmail.com>
2022-11-22 10:28:15 +0700

    wip

---
+++ app/components/blackfriday.tsx
@@ -0,0 +1,7 @@
+export default function BlackFriday() {
+  return (
+    <div className="sticky top-0 text-center bg-rose-500 z-50">
+      Use code <code className="px-2 py-0.5 text-rose-500 bg-white">BLACKFRIDAY</code> for 50% off
+    </div>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,22 +1,17 @@
+import BlackFriday from "~/components/blackfriday";
 import Features from "~/components/features";
 import Hero from "~/components/hero";
 import Pricing from "~/components/pricing";
 
 export default function Index() {
     return (
-        <>
-            <div className="bg-white">
-                <div className="sticky top-0 text-center bg-rose-500 z-50">
-                    Use code <code className="px-2 py-0.5 text-rose-500 bg-white">BLACKFRIDAY</code> for 50% off
-                </div>
-                <main>
-                    <Hero />
-
-                    <Features />
-
-                    <Pricing />
-                </main>
-            </div>
-        </>
+        <div className="bg-white">
+            <BlackFriday />
+            <main>
+                <Hero />
+                <Features />
+                <Pricing />
+            </main>
+        </div>
     );
 }
