cd48bc463e528b16c5e7a56af28c563de82a550e
bangnokia <bangnokia@gmail.com>
2022-11-22 10:22:52 +0700

    update blackfriday coupon

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,6 +6,9 @@ export default function Index() {
     return (
         <>
             <div className="bg-white">
+                <div className="sticky top-0 text-center bg-rose-500 z-50">
+                    Use code <code className="px-2 py-0.5 text-rose-500 bg-white">BLACKFRIDAY</code> for 50% off
+                </div>
                 <main>
                     <Hero />
 
