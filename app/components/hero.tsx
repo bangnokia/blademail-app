ce1cc7c270c1fdbf16f754fd91844eeea1da7dd1
bangnokia <bangnokia@gmail.com>
2023-10-30 12:41:06 +0700

    let user can download free

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,9 +1,40 @@
-import { useState } from "react"
+import {useState} from "react"
 
-export default function Hero() {
+export interface Platform {
+    name: string,
+    size: number,
+    browser_download_url: string
+}
+
+export interface Release {
+    name: string,
+    version: string,
+    description: string,
+    published_at: string,
+    platforms: {
+        "macos": Platform,
+        "win64": Platform,
+        "linux": Platform,
+        "debian": Platform,
+    }
+}
+
+export enum PlatformType {
+    Macos = 'macos',
+    Windows = 'win64',
+    Linux = 'linux',
+    Debian = 'debian'
+};
+
+export default function Hero({release}: { release: Release }) {
+    const [platform, setPlatform] = useState<PlatformType>(PlatformType.Macos);
+
+    const downloadUrl = release.platforms[platform].browser_download_url;
+
     return (
         <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-48">
-            <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
+            <div
+                className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
                 <div>
                     <div className="flex gap-8 items-center">
                         <img
@@ -16,7 +47,8 @@ export default function Hero() {
                     <div className="mt-10">
                         <div className="hidden">
                             <a href="#" className="inline-flex space-x-4">
-                                <span className="rounded bg-rose-50 px-2.5 py-1 text-xs font-semibold text-rose-500 tracking-wide uppercase">
+                                <span
+                                    className="rounded bg-rose-50 px-2.5 py-1 text-xs font-semibold text-rose-500 tracking-wide uppercase">
                                     What's new
                                 </span>
                                 <span className="inline-flex items-center text-sm font-medium text-rose-500 space-x-1">
@@ -42,26 +74,43 @@ export default function Hero() {
                             <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
                                 Desktop App for Email Testing locally
                             </h1>
-                            <p className="mt-6 text-xl text-gray-500">
-                                Never send your email to any free services, Blade Mail will handle emails testing for you.
+                            <p className="mt-6 text-lg text-gray-500">
+                                Never send your email to any free services, Blade Mail will handle emails testing for
+                                you.
                             </p>
                         </div>
-                        <div action="#" className="mt-12 sm:max-w-lg sm:w-full sm:flex">
-                            <div className="flex flex-col gap-4 md:flex-row items-end md:flex-wrap mt-4 sm:mt-0 space-x-5">
+                        <form action="#" className="mt-12 sm:max-w-lg sm:w-full sm:flex">
+                            <div
+                                className="flex flex-col gap-4 mt-4 sm:mt-0">
+                                <div className="flex w-full gap-2">
+
+                                    <select className="w-full md:w-auto border rounded px-5 py-3 bg-white"
+                                            id="platform"
+                                            value={platform}
+                                            onChange={(e) => setPlatform(e.target.value as PlatformType)}
+                                    >
+                                        <option value={PlatformType.Macos}>macOS</option>
+                                        <option value={PlatformType.Windows}>Windows</option>
+                                        <option value={PlatformType.Linux}>Linux</option>
+                                        <option value={PlatformType.Debian}>Debian</option>
+                                    </select>
+                                    <a
+                                        role="button"
+                                        href={downloadUrl}
+                                        className="block w-full transition-colors md:w-auto rounded-md border border-transparent px-5 py-3 bg-yellow-400 text-base font-medium text-white shadow hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 sm:px-10"
+                                    >
+                                        Free download
+                                    </a>
+                                </div>
                                 <a
                                     role="button"
                                     href="https://bangnokia.gumroad.com/l/blade-mail"
-                                    className="block w-full transition-colors md:w-auto rounded-md border border-transparent px-5 py-3 bg-rose-500 text-base font-medium text-white shadow hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 sm:px-10"
+                                    className="block w-full transition-colors md:w-auto rounded-md border border-transparent px-5 py-3 bg-rose-500 text-base text-center font-medium text-white shadow hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 sm:px-10"
                                 >
-                                    Download now
+                                    Buy now
                                 </a>
-
-                                {/* <a href="#" className="text-gray-700 hover:text-rose-500 transition-colors">
-                                    Watch intro video
-                                </a> */}
                             </div>
-                        </div>
-                        <p className="block mt-4 italic text-sky-400">Working on MacOS, Windows, Linux</p>
+                        </form>
                         <div className="mt-6">
                             <div className="inline-flex items-center divide-x divide-gray-300">
                                 <div className="flex-shrink-0 flex pr-5">
@@ -73,7 +122,8 @@ export default function Hero() {
                                         fill="currentColor"
                                         aria-hidden="true"
                                     >
-                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                        <path
+                                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
                                     </svg>
                                     <svg
                                         className="h-5 w-5 text-yellow-400"
@@ -83,7 +133,8 @@ export default function Hero() {
                                         fill="currentColor"
                                         aria-hidden="true"
                                     >
-                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                        <path
+                                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
                                     </svg>
                                     <svg
                                         className="h-5 w-5 text-yellow-400"
@@ -93,7 +144,8 @@ export default function Hero() {
                                         fill="currentColor"
                                         aria-hidden="true"
                                     >
-                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                        <path
+                                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
                                     </svg>
                                     <svg
                                         className="h-5 w-5 text-yellow-400"
@@ -103,7 +155,8 @@ export default function Hero() {
                                         fill="currentColor"
                                         aria-hidden="true"
                                     >
-                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                        <path
+                                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
                                     </svg>
                                     <svg
                                         className="h-5 w-5 text-yellow-400"
@@ -113,7 +166,8 @@ export default function Hero() {
                                         fill="currentColor"
                                         aria-hidden="true"
                                     >
-                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                        <path
+                                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
                                     </svg>
                                 </div>
                                 <div className="min-w-0 flex-1 pl-5 py-1 text-sm text-gray-500 sm:py-3">
@@ -128,7 +182,8 @@ export default function Hero() {
             <div className="sm:mx-auto sm:max-w-3xl sm:px-6">
                 <div className="py-16 sm:relative sm:mt-12 sm:py-12 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
                     <div className="hidden sm:block">
-                        <div className="absolute inset-y-0 left-1/2 w-screen bg-gray-50 rounded-l-3xl lg:left-80 lg:right-0 lg:w-full" />
+                        <div
+                            className="absolute inset-y-0 left-1/2 w-screen bg-gray-50 rounded-l-3xl lg:left-80 lg:right-0 lg:w-full"/>
                         <svg
                             className="absolute top-8 right-1/2 -mr-3 lg:m-0 lg:left-0"
                             width={404}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,12 +3,22 @@ import Faqs from "~/components/faqs";
 import Features from "~/components/features";
 import Hero from "~/components/hero";
 import Pricing from "~/components/pricing";
+import {useLoaderData} from "@remix-run/react";
+import {LoaderFunction} from "remix";
+
+export let loader: LoaderFunction = async () => {
+    let response = await fetch("https://lab.daudau.cc/api/apps/blade-mail/latest-release");
+
+    return response.json();
+}
 
 export default function Index() {
+    const release = useLoaderData();
+
     return (
         <div className="bg-white">
             <main>
-                <Hero />
+                <Hero release={release} />
                 <Features />
                 <Pricing />
                 <Faqs />
